c425375fd54bcc6f1de36fcd7998cb9a5b13a366
VopRos <zolpavel366@gmail.com>
2025-06-15 23:58:12 +0300

    .

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "export",
+    basePath: "/drafts",
     reactStrictMode: false,
     images: {
         remotePatterns: [new URL('https://dvlduuubunqdawuujpgi.supabase.co/storage/v1/object/public/**')],
